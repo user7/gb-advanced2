7920109b2cd8b3ecba5ce8a437c9e7db7bba8fd6
svr <svr@protonmail.com>
2022-04-01 00:11:17 +0300

    перевёл с Dagger на Koin

--- app/src/main/java/com/gb/advanced2/app/App.kt
+++ app/src/main/java/com/gb/advanced2/app/App.kt
@@ -1,19 +1,14 @@
 package com.gb.advanced2.app
 
 import android.app.Application
-import com.gb.advanced2.app.di.AppComponent
-import com.gb.advanced2.app.di.DaggerAppComponent
+import org.koin.core.context.startKoin
 
 class App : Application() {
-    val appComponent: AppComponent by lazy {
-        DaggerAppComponent.builder().build()
-    }
 
-    init {
-        instance = this
-    }
-
-    companion object {
-        lateinit var instance: App
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            modules(koinModule)
+        }
     }
 }
