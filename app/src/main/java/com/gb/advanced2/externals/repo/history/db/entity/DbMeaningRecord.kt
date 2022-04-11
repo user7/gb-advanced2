d44faef0058324ae33832711f2e5238513afacd8
svr <svr.work@protonmail.com>
2022-04-11 19:56:48 +0300

    db wip

---
+++ app/src/main/java/com/gb/advanced2/externals/repo/history/db/entity/DbMeaningRecord.kt
@@ -0,0 +1,9 @@
+package com.gb.advanced2.externals.repo.history.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "meaning")
+class DbMeaningRecord {
+    @PrimaryKey
+}
