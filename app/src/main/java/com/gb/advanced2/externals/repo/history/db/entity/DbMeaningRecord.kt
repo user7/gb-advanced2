524066c0abf21634df6bbbf001772e14f438afc7
svr <svr@protonmail.com>
2022-04-12 14:20:46 +0300

    wip

--- app/src/main/java/com/gb/advanced2/externals/repo/history/db/entity/DbMeaningRecord.kt
+++ app/src/main/java/com/gb/advanced2/externals/repo/history/db/entity/DbMeaningRecord.kt
@@ -1,9 +1,11 @@
 package com.gb.advanced2.externals.repo.history.db.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "meaning")
-class DbMeaningRecord {
-    @PrimaryKey
-}
+data class DbMeaningRecord (
+    @PrimaryKey @ColumnInfo(name = "term_name") val termName : String
+    @ColumnInfo(name = "term_name") val termName : String
+)
