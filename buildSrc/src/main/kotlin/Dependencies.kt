0976c4283f084ed09850f3a6b9aaa3a828583547
svr <svr.work@protonmail.com>
2022-04-12 18:17:16 +0300

    build.gradle.kts

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,9 @@
+object Dependencies {
+    private const val KTX_CORE_VERSION = "1.7.0"
+    const val KTX_CORE = "androidx.core:core-ktx:$KTX_CORE_VERSION"
+
+    private const val ROOM_VERSION = "2.4.2"
+    const val ROOM_RUNTIME = "androidx.room:room-runtime:$ROOM_VERSION"
+    const val ROOM_COMPILER = "androidx.room:room-compiler:$ROOM_VERSION"
+    const val ROOM_KTX = "androidx.room:room-ktx:$ROOM_VERSION"
+}
